1f5cf57809be8f547230457ba02961632488fa10
Your Name <lexa.vergey@gmail.com>
2024-07-10 15:19:03 +0300

    Styles Item component.

--- src/entitys/todo/ui/ToDoItem.tsx
+++ src/entitys/todo/ui/ToDoItem.tsx
@@ -1,11 +1,52 @@
 import { TodoModel } from "../model/types.ts";
+import { Box, Checkbox } from "@mui/material";
+import makeStyles from "@mui/styles/makeStyles";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import { useState } from "react";
 
 interface Props {
   todo: TodoModel;
 }
 
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "dark",
+    minHeight: "7vh",
+    border: "1px solid transparent",
+    boxShadow: "0 4px 2px -2px grey",
+    marginBottom: "5px",
+    fontSize: "20px",
+    fontFamily: "system-ui",
+  },
+  task: {
+    flexGrow: 1,
+    color: "white",
+  },
+  done: {
+    textDecoration: "line-through",
+    color: "gray",
+    flexGrow: 1,
+  },
+}));
+
 const ToDoItem = ({ todo }: Props) => {
-  return <div>{todo.title}</div>;
+  const classes = useStyles();
+  const [isDone, setIsDone] = useState<boolean>(false);
+  const toggle = () => {
+    setIsDone((prevState) => !prevState);
+  };
+
+  return (
+    <Box className={classes.root}>
+      <Checkbox color="default" onClick={toggle} />
+      <Box className={isDone ? classes.done : classes.task}>{todo.title}</Box>
+      <CreateOutlinedIcon sx={{ marginRight: "1rem" }} />
+      <DeleteOutlineOutlinedIcon />
+    </Box>
+  );
 };
 
 export default ToDoItem;
--- src/entitys/todo/ui/ToDoList.tsx
+++ src/entitys/todo/ui/ToDoList.tsx
@@ -1,13 +1,28 @@
 import ToDoItem from "./ToDoItem.tsx";
 import { todoApi } from "../model/api.ts";
+import { Button } from "@mui/material";
+import { useState } from "react";
 
 export const ToDoList = () => {
-  const { data } = todoApi.useTodoListQuery();
+  const [amount, setAmount] = useState(10);
+
+  const { data } = todoApi.useTodoListQuery({
+    limit: amount,
+  });
 
   return (
-    <div>
-      {!!data?.length &&
-        data.map((toDo) => <ToDoItem key={toDo.id} todo={toDo} />)}
-    </div>
+    <>
+      <div>
+        {!!data?.length &&
+          data.map((toDo) => <ToDoItem key={toDo.id} todo={toDo} />)}
+      </div>
+      <Button
+        onClick={() => {
+          setAmount((prev) => prev + 10);
+        }}
+      >
+        More
+      </Button>
+    </>
   );
 };
